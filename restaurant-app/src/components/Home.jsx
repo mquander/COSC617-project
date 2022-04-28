b02af25c3fe9478510f5671bf2123c02c8204b47
sgeese1 <geesey1@live.com>
2022-04-28 18:35:30 -0400

    Initial home page WIP and material ui import. Sam G

--- restaurant-app/src/components/Home.jsx
+++ restaurant-app/src/components/Home.jsx
@@ -1,30 +1,75 @@
 import React from "react";
+import { Button, Dropdown } from "@material-ui/core";
 
 function Home() {
   return (
-    <div className="home">
-      <div class="container">
-        <div class="row align-items-center my-5">
-          <div class="col-lg-7">
-            <img
-              class="img-fluid rounded mb-4 mb-lg-0"
-              src="http://placehold.it/900x400"
-              alt=""
-            />
-          </div>
-          <div class="col-lg-5">
-            <h1 class="font-weight-light">Home</h1>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+    </>
   );
 }
 
 export default Home;
+
+{/* <body>
+    <div className="home">
+    
+    <div class="navbar" id="header-items">
+        <ul id="web-menu">
+            <li><a href="index.html">Home</a></li>
+            
+            <li><button  class="dropdown-btn">Order Online</button></li> 
+                 <div id="myDropdown" class="dropdown-content">
+                   <a href="">Daily Special</a>
+                    <a href="">Weekend Special</a>
+                    <a href="">From the Grill</a>
+                    <a href="">From the Sea</a>
+                </div> 
+            
+            <li><a href="#about">About</a></li>
+            <li><a href="#contactus">Contact Us</a></li>
+            <li><a href="#shoppingcart">Shopping Cart</a></li>
+        </ul>
+    </div>
+<div id="main-content"> 
+    <div id="welcome">   
+        <h1>Welcome to 'restaurant_name'</h1>
+        <h2>Gourmet Italian Cuisine</h2>
+    
+        <div id="fullscreen-image"></div> &nbsp;
+    </div>
+
+    <div class="takeout">
+        <h2>Take Out</h2>
+        
+            <div class="centered">
+                <p>Given the return to prepandemic conditions, </p> 
+                <p>we understand that now everyone is comfortable </p>
+                <p>dining in person. Therefore, we offer to-go options </p>
+                <p>Our takeout options vary</p>
+            </div>
+        
+       
+         <div class="split-right">
+             <div class="centered">
+                 <p>placeholder for image</p>
+             </div>
+        </div>
+    </div> &nbsp;
+    
+   
+    <div id="dine-in">
+        <h2>Limited Dine-In Option</h2>
+        <div class="split-left">
+            
+            <p> Due to limited staffing, </p>
+             <p>please see takeout options</p>
+            
+        </div>
+
+         <div class="split-right">
+            
+        </div>
+        </div>
+    </div> &nbsp;
+</div>
+</body> */}
