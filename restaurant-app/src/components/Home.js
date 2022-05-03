ce047ab650ff7a0183df295d04089a8d329fd5a5
sgeese1 <geesey1@live.com>
2022-05-03 13:37:07 -0400

    Finished home and app bar components, and removed unnecessary files. Sgeesey

--- restaurant-app/src/components/Home.js
+++ restaurant-app/src/components/Home.js
@@ -1,7 +1,33 @@
 import React from "react";
 import CustomAppBar from "./CustomAppBar";
-import { Container, Typography } from "@mui/material";
+import { Container, Typography, Grid, Card, CardHeader, CardContent, Box, CardActions, Button } from "@mui/material";
 
+const options = [
+  {
+    title: 'Menu',
+    description: [
+      'Click here to order some delicious food',
+    ],
+    buttonText: 'Order Now',
+    buttonVariant: 'outlined',
+  },
+  {
+    title: 'About Us',
+    description: [
+      'Click here to learn more about The Restaurant and our story',
+    ],
+    buttonText: 'About',
+    buttonVariant: 'outlined',
+  },
+  {
+    title: 'Contact Us',
+    description: [
+      'Click here to view contact info and details for The Restaurant',
+    ],
+    buttonText: 'Contact us',
+    buttonVariant: 'outlined',
+  },
+];
 
 function Home() {
   return (
@@ -22,6 +48,58 @@ function Home() {
           the highest quality food, coupled with the most effective online ordering 
           system around. Experience The Restaurant today 
         </Typography>
+      </Container>
+      <Container maxWidth="md" component="main">
+        <Grid container spacing={5} alignItems="flex-end">
+          {options.map((option) => (
+            <Grid
+              item
+              key={option.title}
+              xs={12}
+              sm={12}
+              md={4}
+            >
+              <Card>
+                <CardHeader
+                  title={option.title}
+                  titleTypographyProps={{ align: 'center' }}
+                  sx={{
+                    backgroundColor: (theme) =>
+                      theme.palette.mode === 'light'
+                        ? theme.palette.grey[200]
+                        : theme.palette.grey[700],
+                  }}
+                />
+                <CardContent>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'center',
+                      alignItems: 'baseline',
+                      mb: 2,
+                    }}
+                  >
+                  </Box>
+                  <p>
+                      <Typography
+                        component="p"
+                        variant="body1"
+                        align="center"
+                        key={option.description}
+                      >
+                        {option.description}
+                      </Typography>
+                  </p>
+                </CardContent>
+                <CardActions>
+                  <Button fullWidth variant={option.buttonVariant}>
+                    {option.buttonText}
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
       </Container>
     </>
   );
--- restaurant-app/src/index.js
+++ restaurant-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Home, CustomAppBar } from "./components"
+import { Home } from "./components/Paths"
 
 ReactDOM.render(
 <Router>
