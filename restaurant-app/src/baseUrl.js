9a970eba86659598ccaca84e3fb1362453ec4545
sgeese1 <geesey1@live.com>
2022-05-09 19:01:00 -0400

    Removed extra backslash

--- restaurant-app/src/baseUrl.js
+++ restaurant-app/src/baseUrl.js
@@ -1,5 +1,5 @@
 
 const baseUrl = process.env.NODE_ENV === "production"
-? "https://tu-cosc-617-final-project.herokuapp.com/"
+? "https://tu-cosc-617-final-project.herokuapp.com"
 : "http://localhost:5000";
 export default baseUrl;
